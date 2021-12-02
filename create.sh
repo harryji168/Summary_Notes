e5aa4607c2ed5ba4afc554a035288ddef04959fd
Harry Ji <jiharry@hotmail.com>
2021-12-01 18:57:28 -0800

    Wed 01 Dec 2021 06:57:28 PM PST

--- create.sh
+++ create.sh
@@ -4,6 +4,7 @@ git config --global credential.helper store
 git init
 git add .
 git commit -m  "`date`"
+git checkout -b main
 git branch -M main
 git remote add origin $1
 git push -u origin main --force
