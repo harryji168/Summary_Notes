06f1b7b7ff159b012fc835f825834d78ecabebf8
Harry Ji <jiharry@hotmail.com>
2022-01-23 18:21:15 -0800

    Sun 23 Jan 2022 06:21:15 PM PST

--- build.sh
+++ build.sh
@@ -18,6 +18,13 @@ git config  user.email "jiharry@hotmail.com"
 git config  user.name "Harry Ji"
 git config  credential.helper store
 
+git add .
+git commit -m  "`date`"
+
+git checkout -b main
+git branch -M main
+git remote add origin $1
+git push -u origin main --force
 
 git init
 git add -A
