200b688497e65a530adca3f3e251608da10cba6b
Harry Ji <jiharry@hotmail.com>
2022-01-23 18:27:14 -0800

    Sun 23 Jan 2022 06:27:14 PM PST

--- build.sh
+++ build.sh
@@ -1,41 +1,13 @@
-#!/usr/bin/env sh
-
-# abort on errors
-set -e
-
-# build
-npm run deploy
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 rm -rf .git
 git init
 git config  user.email "jiharry@hotmail.com"
 git config  user.name "Harry Ji"
 git config  credential.helper store
+curl https://www.toptal.com/developers/gitignore/api/node,linux,rails,macos,windows > .gitignore
 
 git add .
 git commit -m  "`date`"
-
-git checkout -b main
-git branch -M main
-git remote add origin $1
-git push -u origin main --force
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:harryji168/vuejs.git main:gh-pages
 git checkout -b gh-pages
+git branch -M gh-pages
 git remote add origin $1
 git push -u origin gh-pages --force
-cd -
