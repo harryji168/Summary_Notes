e9e820dcbe482ac89faed2282845819690665389
Harry Ji <jiharry@hotmail.com>
2022-01-23 18:18:14 -0800

    Sun 23 Jan 2022 06:18:14 PM PST

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run deploy
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+rm -rf .git
+git init
+git config  user.email "jiharry@hotmail.com"
+git config  user.name "Harry Ji"
+git config  credential.helper store
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:harryji168/vuejs.git main:gh-pages
+git checkout -b gh-pages
+git remote add origin $1
+git push -u origin gh-pages --force
+cd -
