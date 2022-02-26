9ae7a0851e851cf4f7df4341267f79031fb60ec9
Harry Ji <jiharry@hotmail.com>
2022-02-26 12:03:19 -0800

    Sat 26 Feb 2022 12:03:19 PM PST

--- PHP/codelgniter/migrate.php
+++ PHP/codelgniter/migrate.php
@@ -1,3 +1,10 @@
 https://www.tech-prastish.com/blog/how-to-use-migrations-in-codeigniter-3/
 
 php index.php migrate
+
+
+https://stackoverflow.com/questions/33612956/codeigniter-fatal-error-call-to-undefined-function-mysqli-init
+
+extension=php_mysqli.dll
+
+sudo systemctl restart apache2
