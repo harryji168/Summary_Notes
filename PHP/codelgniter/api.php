377bc2208492b759031c7a1d0f9bd315248bb68d
Harry Ji <jiharry@hotmail.com>
2022-02-26 17:39:20 -0800

    Sat 26 Feb 2022 05:39:20 PM PST

--- PHP/codelgniter/api.php
+++ PHP/codelgniter/api.php
@@ -3,3 +3,4 @@ https://www.abhijitgatade.com/blogs/view/restful-web-api-in-codeigniter-3
 sudo chmod -R 755 /var/www/your_domain/application/libraries/REST_Controller.php
 
 sudo chmod -R 755 /var/www/your_domain/application/config/rest.php
+sudo chmod -R 755 /var/www/your_domain/application/libraries/Format.php
