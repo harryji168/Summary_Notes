20a9d7cdd7d326a384c9b44e214e6752533149c1
Harry Ji <jiharry@hotmail.com>
2022-02-26 17:42:19 -0800

    Sat 26 Feb 2022 05:42:19 PM PST

--- PHP/codelgniter/api.php
+++ PHP/codelgniter/api.php
@@ -4,3 +4,4 @@ sudo chmod -R 755 /var/www/your_domain/application/libraries/REST_Controller.php
 
 sudo chmod -R 755 /var/www/your_domain/application/config/rest.php
 sudo chmod -R 755 /var/www/your_domain/application/libraries/Format.php
+sudo chmod -R 755 /var/www/your_domain/system/language/english/rest_controller_lang.php
