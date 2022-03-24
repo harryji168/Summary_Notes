528d974410bb6bd83d955caf5e54089c1985885c
Harry Ji <jiharry@hotmail.com>
2022-03-24 12:24:19 -0700

    Thu 24 Mar 2022 12:24:19 PM PDT

--- PHP/php_version_switch.php
+++ PHP/php_version_switch.php
@@ -3,3 +3,5 @@ https://ostechnix.com/how-to-switch-between-multiple-php-versions-in-ubuntu/
 $ sudo update-alternatives --config php
 
 sudo systemctl restart apache2
+
+/etc/php/7.4/apache2/php.ini
