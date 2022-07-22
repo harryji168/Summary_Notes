5417a2b84996a8a5fcc21fd42e3cd2467847860a
Harry Ji <jiharry@hotmail.com>
2022-07-21 21:48:12 -0700

    Thu 21 Jul 2022 09:48:12 PM PDT

--- PHP/php_version_switch.php
+++ PHP/php_version_switch.php
@@ -12,3 +12,15 @@ https://www.rosehosting.com/blog/how-to-install-php-7-4-with-nginx-on-ubuntu-20-
  
 
 https://www.cloudbooklet.com/install-php-7-4-on-ubuntu/
+
+
+$ git clone https://github.com/Bikranshu/lumen-react.git
+ $ cd lumen-react
+ $ composer install
+ $ npm install
+ $ cp .env.example .env (edit it with your database information)
+ $ php artisan jwt:secret
+ $ php artisan migrate
+ $ php artisan db:seed
+ $ php -S localhost:8000 -t public/
+ $ npm run watch
